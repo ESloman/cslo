a2f3d4236c01b60647962bfca130aff9830a15ca
esloman <elliot.sloman@hotmail.com>
2023-11-26 21:06:23 +0000

    Added basic parser, compiler and vm

---
+++ src/compiler.h
@@ -0,0 +1,8 @@
+#ifndef cslo_compiler_h
+#define cslo_compiler_h
+
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
+
+#endif
---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef cslo_debug_h
+#define cslo_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
